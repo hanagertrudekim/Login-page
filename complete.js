9aded6af69c9065a305c5c68ed98309bad503083
hana Kim <rlagksk8428@gmail.com>
2022-05-25 21:48:18 +0900

    ㅁ

--- complete.js
+++ complete.js
@@ -5,6 +5,7 @@ const name = localStorage.getItem("ID");
 if (localStorage.getItem("loginFlag") === "true") {
   hello.innerText = `안녕하세요. ${name}님!`;
 } else {
+  alert("접근할수없는 사용자입니다.");
   window.location.replace("index.html");
 }
 
